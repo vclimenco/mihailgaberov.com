159cb364dfd3df891fe1f0d78dfcebbdb8d4f2b6
mihail.gaberov <mihail.gaberov@ysura.com>
2022-03-07 10:29:00 +0100

    :sparkles: Use values from package.json

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 import styles from "./Header.module.scss";
 import { ThemeChanger } from "../ThemeChanger";
+const packageJson = require("../../package.json");
 
 export const Header = () => {
   return (
-      <header className={styles.header}>
-        <div className={styles.logos}>
-          <h1>mihail gaberov</h1>
-          <sub>Learning by sharing.</sub>
-        </div>
-        <ThemeChanger/>
-      </header>
+    <header className={styles.header}>
+      <div className={styles.logos}>
+        <h1>{packageJson.author.toLowerCase()}</h1>
+        <sub>Learning by sharing.</sub>
+      </div>
+      <ThemeChanger />
+    </header>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -4,52 +4,55 @@ import { getDatabase } from "../lib/notion";
 import { Text } from "./[id].js";
 import styles from "./index.module.scss";
 import { Header } from "../components/Header";
-
+const packageJson = require("../package.json");
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
 export default function Home({ posts }) {
   return (
-      <div>
-        <Header />
-        <Head>
-          <title>Mihail Gaberov</title>
-          <link rel="icon" href="/favicon.ico"/>
-        </Head>
+    <div>
+      <Header />
+      <Head>
+        <title>{packageJson.author}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main className={styles.container}>
-          <h2 className={styles.heading}>All Posts</h2>
-          <ol className={styles.posts}>
-            {posts.map((post) => {
-              const date = new Date(post.last_edited_time).toLocaleString(
-                  "en-US",
-                  {
-                    month: "short",
-                    day: "2-digit",
-                    year: "numeric",
-                  }
-              );
+      <main className={styles.container}>
+        <h2 className={styles.heading}>All Posts</h2>
+        <ol className={styles.posts}>
+          {posts.map((post) => {
+            const date = new Date(post.last_edited_time).toLocaleString(
+              "en-US",
+              {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+              }
+            );
 
-              return (
-                  <li key={post.id} className={styles.post}>
-                    <h3 className={styles.postTitle} key={post.id}>
-                      <Link key={post.id} href={`/${post.id}`}>
-                        <a key={post.id}>
-                          <Text text={post.properties.Name.title} postId={post.id}/>
-                        </a>
-                      </Link>
-                    </h3>
+            return (
+              <li key={post.id} className={styles.post}>
+                <h3 className={styles.postTitle} key={post.id}>
+                  <Link key={post.id} href={`/${post.id}`}>
+                    <a key={post.id}>
+                      <Text
+                        text={post.properties.Name.title}
+                        postId={post.id}
+                      />
+                    </a>
+                  </Link>
+                </h3>
 
-                    <p className={styles.postDescription}>{date}</p>
-                    <Link href={`/${post.id}`}>
-                      <a> Read post →</a>
-                    </Link>
-                  </li>
-              );
-            })}
-          </ol>
-        </main>
-      </div>
+                <p className={styles.postDescription}>{date}</p>
+                <Link href={`/${post.id}`}>
+                  <a> Read post →</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ol>
+      </main>
+    </div>
   );
 }
 
