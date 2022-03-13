4a85d3470351513a11c4949cf1fa0d6d9679b758
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-03-13 11:54:43 +0100

    :construction: Adding syntax highlighting

--- pages/[id].js
+++ pages/[id].js
@@ -1,10 +1,13 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import Head from "next/head";
-import { getDatabase, getPage, getBlocks } from "../lib/notion";
 import Link from "next/link";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+
+import { getBlocks, getDatabase, getPage } from "../lib/notion";
 import { databaseId } from "./index.js";
 import styles from "./post.module.css";
-import {Layout} from "../components/Layout";
+import { Layout } from "../components/Layout";
 
 export const Text = ({ text, postId }) => {
   if (!text) {
@@ -16,17 +19,17 @@ export const Text = ({ text, postId }) => {
       text,
     } = value;
     return (
-      <span
-        className={[
-          bold ? styles.bold : "",
-          code ? styles.code : "",
-          italic ? styles.italic : "",
-          strikethrough ? styles.strikethrough : "",
-          underline ? styles.underline : "",
-        ].join(" ")}
-        style={color !== "default" ? { color } : {}}
-        key={postId + value.plain_text}
-      >
+        <span
+            className={[
+              bold ? styles.bold : "",
+              code ? styles.code : "",
+              italic ? styles.italic : "",
+              strikethrough ? styles.strikethrough : "",
+              underline ? styles.underline : "",
+            ].join(" ")}
+            style={color !== "default" ? { color } : {}}
+            key={postId + value.plain_text}
+        >
         {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
       </span>
     );
@@ -40,128 +43,138 @@ const renderBlock = (block) => {
   switch (type) {
     case "paragraph":
       return (
-        <p>
-          <Text text={value.text} />
-        </p>
+          <p>
+            <Text text={value.text}/>
+          </p>
       );
     case "heading_1":
       return (
-        <h1>
-          <Text text={value.text} />
-        </h1>
+          <h1>
+            <Text text={value.text}/>
+          </h1>
       );
     case "heading_2":
       return (
-        <h2>
-          <Text text={value.text} />
-        </h2>
+          <h2>
+            <Text text={value.text}/>
+          </h2>
       );
     case "heading_3":
       return (
-        <h3>
-          <Text text={value.text} />
-        </h3>
+          <h3>
+            <Text text={value.text}/>
+          </h3>
       );
     case "bulleted_list_item":
     case "numbered_list_item":
       return (
-        <li>
-          <Text text={value.text} />
-        </li>
+          <li>
+            <Text text={value.text}/>
+          </li>
       );
     case "to_do":
       return (
-        <div>
-          <label htmlFor={id}>
-            <input type="checkbox" id={id} defaultChecked={value.checked} />{" "}
-            <Text text={value.text} />
-          </label>
-        </div>
+          <div>
+            <label htmlFor={id}>
+              <input type="checkbox" id={id} defaultChecked={value.checked}/>{" "}
+              <Text text={value.text}/>
+            </label>
+          </div>
       );
     case "toggle":
       return (
-        <details>
-          <summary>
-            <Text text={value.text} />
-          </summary>
-          {value.children?.map((block) => (
-            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
-          ))}
-        </details>
+          <details>
+            <summary>
+              <Text text={value.text}/>
+            </summary>
+            {value.children?.map((block) => (
+                <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+            ))}
+          </details>
       );
     case "child_page":
       return <p>{value.title}</p>;
     case "image":
       const src =
-        value.type === "external" ? value.external.url : value.file.url;
+          value.type === "external" ? value.external.url : value.file.url;
       const caption = value.caption ? value.caption[0]?.plain_text : "";
       return (
-        <figure>
-          <img src={src} alt={caption} />
-          {caption && <figcaption>{caption}</figcaption>}
-        </figure>
+          <figure>
+            <img src={src} alt={caption}/>
+            {caption && <figcaption>{caption}</figcaption>}
+          </figure>
       );
     case "divider":
-      return <hr key={id} />;
+      return <hr key={id}/>;
     case "quote":
       return <blockquote key={id}>{value.text[0].plain_text}</blockquote>;
     case "code":
-      return (
-        <pre className={styles.pre}>
-          <code className={styles.code_block} key={id}>
-            {value.text[0].plain_text}
-          </code>
-        </pre>
-      );
+      return value.text?.map((t) => {
+        if (!t.plain_text) return '';
+
+        return (
+            <pre className={styles.pre}>
+            <code className={`language-${block.code.language}`} key={id}>
+              {t.plain_text}
+            </code>
+          </pre>
+        );
+      });
     case "file":
       const src_file =
-        value.type === "external" ? value.external.url : value.file.url;
+          value.type === "external" ? value.external.url : value.file.url;
       const splitSourceArray = src_file.split("/");
       const lastElementInArray = splitSourceArray[splitSourceArray.length - 1];
       const caption_file = value.caption ? value.caption[0]?.plain_text : "";
       return (
-        <figure>
-          <div className={styles.file}>
-            📎{" "}
-            <Link href={src_file} passHref>
-              {lastElementInArray.split("?")[0]}
-            </Link>
-          </div>
-          {caption_file && <figcaption>{caption_file}</figcaption>}
-        </figure>
+          <figure>
+            <div className={styles.file}>
+              📎{" "}
+              <Link href={src_file} passHref>
+                {lastElementInArray.split("?")[0]}
+              </Link>
+            </div>
+            {caption_file && <figcaption>{caption_file}</figcaption>}
+          </figure>
       );
     default:
       return `❌ Unsupported block (${
-        type === "unsupported" ? "unsupported by Notion API" : type
+          type === "unsupported" ? "unsupported by Notion API" : type
       })`;
   }
 };
 
 export default function Post({ page, blocks }) {
   if (!page || !blocks) {
-    return <div />;
+    return <div/>;
   }
-  return (
-    <Layout>
-      <Head>
-        <title>{page.properties.Name.title[0].plain_text}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <article className={styles.container}>
-        <h1 className={styles.name}>
-          <Text text={page.properties.Name.title} />
-        </h1>
-        <section>
-          {blocks.map((block) => (
-            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
-          ))}
-          <Link href="/">
-            <a className={styles.back}>← Go home</a>
-          </Link>
-        </section>
-      </article>
-    </Layout>
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
+
+  return (
+      <Layout>
+        <Head>
+          <title>{page.properties.Name.title[0].plain_text}</title>
+          <link rel="icon" href="/favicon.ico"/>
+        </Head>
+
+        <article className={styles.container}>
+          <h1 className={styles.name}>
+            <Text text={page.properties.Name.title}/>
+          </h1>
+          <section>
+            {blocks.map((block) => (
+                <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+            ))}
+            <Link href="/">
+              <a className={styles.back}>← Go home</a>
+            </Link>
+          </section>
+        </article>
+      </Layout>
   );
 }
 
@@ -181,20 +194,20 @@ export const getStaticProps = async (context) => {
   // Retrieve block children for nested blocks (one level deep), for example toggle blocks
   // https://developers.notion.com/docs/working-with-page-content#reading-nested-blocks
   const childBlocks = await Promise.all(
-    blocks
-      .filter((block) => block.has_children)
-      .map(async (block) => {
-        return {
-          id: block.id,
-          children: await getBlocks(block.id),
-        };
-      })
+      blocks
+          .filter((block) => block.has_children)
+          .map(async (block) => {
+            return {
+              id: block.id,
+              children: await getBlocks(block.id),
+            };
+          })
   );
   const blocksWithChildren = blocks.map((block) => {
     // Add child blocks if the block should contain children but none exists
     if (block.has_children && !block[block.type].children) {
       block[block.type]["children"] = childBlocks.find(
-        (x) => x.id === block.id
+          (x) => x.id === block.id
       )?.children;
     }
     return block;
