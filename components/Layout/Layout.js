69b93b5f40b719a6161e2db252a611055881ab19
mihail.gaberov <mihail.gaberov@ysura.com>
2022-03-24 13:14:53 +0100

    👷🏻‍♂️ Adding Walking Man animation component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,21 +3,11 @@ import { Navigation } from "../Navigation";
 import { Footer } from "../Footer";
 import styles from "../../pages/index.module.scss";
 import { WalkingMan } from "../WalkingMan";
-import { useState } from "react";
-import useInterval from "../../hooks/useInterval.hook";
-
-const ANIMATION_INTERVAL = 10000;
 
 export const Layout = ({ children }) => {
-  const [showWalkingMan, setShowWalkingMan] = useState(false);
-
-  useInterval(() => {
-    setShowWalkingMan(!showWalkingMan);
-  }, ANIMATION_INTERVAL);
-
   return (
     <>
-      {showWalkingMan && <WalkingMan />}
+      <WalkingMan />
       <Header />
       <main className={styles.container}>
         <Navigation />
