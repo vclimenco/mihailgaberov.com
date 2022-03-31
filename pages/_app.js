f29de6a8e39b3fefd5046d45367900058a4682b2
mihail.gaberov <mihail.gaberov@ysura.com>
2022-03-31 09:01:43 +0200

    ✨ Add css resest and update styling accordingly

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
+import "../styles/reset.css";
 import "../styles/globals.css";
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
