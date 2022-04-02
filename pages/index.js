2b9869042c66219a44562af5772a722da0801da1
mihailgaberov <mihail.gaberov@gmail.com>
2022-04-02 12:43:19 +0200

    :sparkles: Slugify the URLs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import slugify from "slugify";
 
 import { Layout } from "../components/Layout";
 import { getDatabase } from "../lib/notion";
 import styles from "./index.module.scss";
-import { Text } from "./[id].js";
+import { Text } from "./[slug].js";
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
@@ -30,10 +31,12 @@ export default function Home({ posts }) {
             }
           );
 
+          const title = post.properties.Name.title[0].plain_text;
+
           return (
             <li key={post.id} className={styles.post}>
               <h3 className={styles.postTitle}>
-                <Link href={`/${post.id}`}>
+                <Link href={`/${slugify(title).toLowerCase()}`}>
                   <a>
                     <Text text={post.properties.Name.title} postId={post.id} />
                   </a>
@@ -41,7 +44,7 @@ export default function Home({ posts }) {
               </h3>
 
               <p className={styles.postDescription}>{date}</p>
-              <Link href={`/${post.id}`}>
+              <Link href={`/${slugify(title).toLowerCase()}`}>
                 <a> Read post →</a>
               </Link>
             </li>
