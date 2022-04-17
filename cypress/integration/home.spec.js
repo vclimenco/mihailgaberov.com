2f488ec86050dbff37d37542a8384c59e10ca17e
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-04-17 13:06:35 +0200

    :sparkles: Adding Cypressjs

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,38 @@
+describe('Home page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it("greets logo", () => {
+    cy.contains('h2', 'mihail  gaberov')
+  })
+
+  /*it("links to #/register", () => {
+    cy
+        .contains('Don\'t have an account? Register here >')
+        .should('have.attr', 'href', '#/register')
+  })
+
+  it("requires email", () => {
+    cy.get('form').contains('Login').click()
+    cy.get("input:invalid").should("have.length", 2)
+  })
+
+  it("requires password", () => {
+    cy.get('[data-cy=password]').type('test@test.com{enter}')
+    cy.get("input:invalid").should("have.length", 1)
+  })
+
+  it("requires valid user and password", () => {
+    cy.get('[data-cy=email]').type('test@test.com')
+    cy.get('[data-cy=password]').type('password{enter}')
+    cy.get('.error')
+        .should('contain', 'Your account is not activated yet.')
+  })
+
+  it("navigates to #/ on successful login", () => {
+    cy.get('[data-cy=email]').type('mihail.gaberov@gmail.com')
+    cy.get('[data-cy=password]').type('asdasdasd{enter}')
+    cy.hash().should('eq', '#/')
+  })*/
+})
